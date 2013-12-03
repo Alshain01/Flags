51469f7f952005d25abb5445ff7e5b073f7ee586
Kevin Seiden <alshain01@suddenlink.net>
2013-12-03 13:30:29 -0600

    Create distribution management

--- Flags/src/main/java/io/github/alshain01/Flags/Updater.java
+++ Flags/src/main/java/io/github/alshain01/Flags/Updater.java
@@ -60,7 +60,7 @@ final class Updater {
     private static final String QUERY = "/servermods/files?projectIds="; // Path to GET
     private static final String HOST = "https://api.curseforge.com"; // Slugs will be appended to this to get to the project's RSS feed
 
-    private static final String[] NO_UPDATE_TAG = { "-ALPHA", "-BETA" }; // If the version number contains one of these, don't update.
+    private static final String[] NO_UPDATE_TAG = { "-ALPHA", "-BETA", "-SNAPSHOT" }; // If the version number contains one of these, don't update.
     private static final int BYTE_SIZE = 1024; // Used for downloading files
     private String updateFolder;// The folder that downloads will be placed in
     private Updater.UpdateResult result = Updater.UpdateResult.SUCCESS; // Used for determining the outcome of the update process
