12c313df9e0cd654921ef1789a7ccb2b89120f95
Kevin Seiden <alshain01@suddenlink.net>
2014-04-17 21:26:53 -0500

    Fixes missing inheritance handling

--- src/main/java/io/github/alshain01/flags/CommandBase.java
+++ src/main/java/io/github/alshain01/flags/CommandBase.java
@@ -258,36 +258,20 @@ abstract class CommandBase {
         return (value) ? Message.VALUE_COLOR_TRUE.get() : Message.VALUE_COLOR_FALSE.get();
     }
 
-    private static Area getArea(Location loc, CommandLocation location, boolean adjustInheritance) {
+    static Area getArea(Location loc, CommandLocation location) {
         if (location == CommandLocation.DEFAULT) {
             return new AreaDefault(loc.getWorld());
         } else if (location == CommandLocation.WILDERNESS) {
             return new AreaWilderness(loc.getWorld());
         } else if (location == CommandLocation.AREA) {
-            Area area = FlagsAPI.getAreaAt(loc);
-            if (area instanceof AreaWilderness) return null;
-            if(adjustInheritance && area instanceof Subdividable) {
-                Subdividable sub = (Subdividable) area;
-                if (sub.isSubdivision() && sub.isInherited())
-                    area = ((Subdividable) area).getParent();
-            }
-
-            return area;
+            if(FlagsAPI.hasArea(loc)) return FlagsAPI.getAreaAt(loc);
         }
         // Invalid location selection
         return null;
     }
 
-    static Area getArea(Location loc, CommandLocation location) {
-        return getArea(loc, location, true);
-    }
-
-    static Area getAbsoluteArea(Player player, CommandLocation location) {
-        return getArea(player.getLocation(), location, false);
-    }
-
     static Area getArea(Player player, CommandLocation location) {
-        return getArea(player.getLocation(), location, true);
+        return getArea(player.getLocation(), location);
     }
 
     static Set<Player> getPlayerList(Player player, Set<String> playerNames) {
